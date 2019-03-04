118738484f23c686ba8c80d752779bfc4660b920
idzhalalov <idzhalalov@gmail.com>
2019-03-04 20:01:44 +0500

    Questions routes

--- routes.py
+++ routes.py
@@ -1,1 +1,7 @@
-routes = []
+from api.views import QuestionHandler
+
+routes = [
+    ('GET', '/api/v1/questions/{pk:\d+}', QuestionHandler, 'question_obj'),
+    ('GET', '/api/v1/questions', QuestionHandler, 'question'),
+    ('POST', '/api/v1/questions', QuestionHandler, 'question'),
+]
